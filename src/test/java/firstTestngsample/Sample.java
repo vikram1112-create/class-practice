4e39d79e3b4081284d853f6fff044b055007beee
vikram1112-create <vikramkrishna22@gmail.com>
2025-08-28 14:36:25 +0530

    hello

---
+++ src/test/java/firstTestngsample/Sample.java
@@ -0,0 +1,30 @@
+package firstTestngsample;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	
+   @Test(priority=-7)
+	private void tc01() {
+		System.out.println("test1");
+
+	}
+	@Test(priority=-2)	
+     private void tc02() {
+	System.out.println("test2");
+
+}
+    @Test(priority=1) 
+     private void tc03() {
+		System.out.println("test3");
+
+	}
+     @Test(priority=7)
+     private void tc04() {
+		System.out.println("test4");
+
+	}
+	}
+
+
